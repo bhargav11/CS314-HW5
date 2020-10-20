d130e5d737b7de7d9506aa86db9648fcabaefefe
Bhargav R. Singaraju <brs110@ice.cs.rutgers.edu>
2020-10-20 02:53:38 -0400

    prob 1 done

--- HW5.hs
+++ HW5.hs
@@ -3,4 +3,11 @@ module HW5 where
 data Tree a = Tip | Bin (Tree a) a (Tree a) deriving (Show, Eq)
 data Rose a = Node a [Rose a] deriving (Show, Eq)
 
+depthFirstInOrder :: Tree a -> [a]
+depthFirstInOrder (Bin l v r) = (depthFirstInOrder l) ++ [v] ++ (depthFirstInOrder r)
+
+fromTree :: Tree a -> [a]
+fromTree (Bin l x r) = ( depthFirstInOrder (Bin l x r) )
+
+--trunc :: Int -> Tree a -> Tree a
 
